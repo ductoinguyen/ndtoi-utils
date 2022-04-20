628d89dad2fe55ccdcdffa77f83ac685986034c6
ductoinguyen <toinguyenduc@admicro.vn>
2022-04-20 14:23:36 +0700

    init project

---
+++ src/main/java/com/ndtoi/numberic/NumbericUtils.java
@@ -0,0 +1,13 @@
+package com.ndtoi.numberic;
+
+/**
+ * @author Nguyễn Đức Tới (ndtoi)
+ * Ngày tạo 20/04/2022
+ */
+public class NumbericUtils {
+    private NumbericUtils() {}
+
+    public static boolean isNumber(String checkNumber) {
+        return true;
+    }
+}
